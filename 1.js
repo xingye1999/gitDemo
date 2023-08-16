eacdbb8e677a9090ce8e11d84a4b69eeedb3ea8f
chenzhihao <chenzhihao@dxy.cn>
2023-08-16 10:19:28 +0800

    1.js

---
+++ 1.js
@@ -0,0 +1,14 @@
+1.beforeEnter(el) {
+    console.log('beforeEnter');
+},
+enter(el, done) {
+    console.log('enter');
+    done();
+},
+beforeLeave(el) {
+    console.log('beforeLeave');
+},
+leave(el, done) {
+    console.log('leave');
+    done();
+},
